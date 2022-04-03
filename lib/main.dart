ed935c3111803ded05421dc2b15271ded2e8d68f
iron <brightlyiron@gmail.com>
2022-04-03 17:35:56 +0900

    :hammer:Add(ColumnRow) column, row, expanded, flaxible widget 학습

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splash_screen/screen/home_screen.dart';
 
 void main() {
   runApp(
@@ -9,7 +10,7 @@ void main() {
   );
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeSplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Flexible(
+                child: Container(
+                  color: Colors.red,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
